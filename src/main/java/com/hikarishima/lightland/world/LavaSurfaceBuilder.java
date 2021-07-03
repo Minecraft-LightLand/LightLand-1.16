c4a9850efa7bb3421e5dd2d5b3db516f0027d060
lcy0x1 <arthurlcy0x1@gmail.com>
2021-07-02 21:30:45 -0700

    add new biome files

--- src/main/java/com/hikarishima/lightland/world/LavaSurfaceBuilder.java
+++ src/main/java/com/hikarishima/lightland/world/LavaSurfaceBuilder.java
@@ -43,25 +43,31 @@ public class LavaSurfaceBuilder extends SurfaceBuilder<SurfaceBuilderConfig> {
         this.apply(p_205610_1_, p_205610_2_, p_205610_3_, p_205610_4_, p_205610_5_, p_205610_6_, p_205610_7_, p_205610_9_, p_205610_10_, p_205610_14_.getTopMaterial(), p_205610_14_.getUnderMaterial(), p_205610_14_.getUnderwaterMaterial(), p_205610_11_);
     }
 
-    protected void apply(Random r, IChunk c, Biome b, int x, int z, int y, double v, BlockState b0, BlockState b1, BlockState c0, BlockState c1, BlockState c2, int top) {
+    protected void apply(Random r, IChunk c, Biome b,
+                         int x, int z, int y, double v,
+                         BlockState solid, BlockState liquid,
+                         BlockState c0, BlockState c1, BlockState c2,
+                         int top) {
+
         BlockState ic0 = c0;
         BlockState ic1 = c1;
+
         BlockPos.Mutable pos = new BlockPos.Mutable();
         int depth = -1;
-        int thickness = (int)(v / 3.0D + 3.0D + r.nextDouble() * 0.25D);
-        int cx = x & 15;
-        int cy = z & 15;
+        final int thickness = (int)(v / 3.0D + 3.0D + r.nextDouble() * 0.25D);
+        final int cx = x & 15;
+        final int cz = z & 15;
 
         for(int iy = y; iy >= 0; --iy) {
-            pos.set(cx, iy, cy);
-            BlockState current = c.getBlockState(pos);
+            pos.set(cx, iy, cz);
+            final BlockState current = c.getBlockState(pos);
             if (current.isAir()) {
                 depth = -1;
-            } else if (current.is(b0.getBlock())) {
+            } else if (current.is(solid.getBlock())) {
                 if (depth == -1) {
                     if (thickness <= 0) {
                         ic0 = Blocks.AIR.defaultBlockState();
-                        ic1 = b0;
+                        ic1 = solid;
                     } else if (iy >= top - 4 && iy <= top + 1) {
                         ic0 = c0;
                         ic1 = c1;
@@ -71,10 +77,10 @@ public class LavaSurfaceBuilder extends SurfaceBuilder<SurfaceBuilderConfig> {
                         if (b.getTemperature(pos.set(x, iy, z)) < 0.15F) {
                             ic0 = Blocks.ICE.defaultBlockState();
                         } else {
-                            ic0 = b1;
+                            ic0 = liquid;
                         }
 
-                        pos.set(cx, iy, cy);
+                        pos.set(cx, iy, cz);
                     }
 
                     depth = thickness;
@@ -82,7 +88,7 @@ public class LavaSurfaceBuilder extends SurfaceBuilder<SurfaceBuilderConfig> {
                         c.setBlockState(pos, ic0, false);
                     } else if (iy < top - 7 - thickness) {
                         ic0 = Blocks.AIR.defaultBlockState();
-                        ic1 = b0;
+                        ic1 = solid;
                         c.setBlockState(pos, c2, false);
                     } else {
                         c.setBlockState(pos, ic1, false);
