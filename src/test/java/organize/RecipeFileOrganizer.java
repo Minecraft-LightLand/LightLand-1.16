c8d82baf49b4ed85cd844355505f769fa80cced1
lcy0x1 <arthurlcy0x1@gmail.com>
2021-07-21 06:30:44 -0700

    add recipe generation

---
+++ src/test/java/organize/RecipeFileOrganizer.java
@@ -0,0 +1,40 @@
+package organize;
+
+import com.google.common.io.Files;
+
+import java.io.File;
+
+public class RecipeFileOrganizer extends ResourceOrganizer {
+
+    public RecipeFileOrganizer() {
+        super(Type.DATA, "recipes", "recipes/");
+    }
+
+
+    @Override
+    public void organize(File f) throws Exception {
+        process("", f);
+    }
+
+    private void process(String prefix, File f) throws Exception {
+        String filename = f.getName();
+        if (filename.startsWith("-") || filename.startsWith("."))
+            return;
+        filename = f.isDirectory() ? filename : filename.split("\\.")[0];
+        String name = filename.startsWith("_") ? prefix + filename : filename.endsWith("_") ? filename + prefix : filename;
+        if (f.isDirectory()) {
+            for (File fi : f.listFiles()) {
+                String file = fi.getName();
+                if (file.startsWith("-") || file.startsWith("."))
+                    continue;
+                process(name, fi);
+            }
+            return;
+        }
+        File ti = new File(getTargetFolder() + name + ".json");
+        check(ti);
+        Files.copy(f, ti);
+    }
+
+
+}
--- src/test/java/organize/ResourceOrganizer.java
+++ src/test/java/organize/ResourceOrganizer.java
@@ -32,10 +32,12 @@ public abstract class ResourceOrganizer {
     public static void main(String[] args) throws Exception {
         delete(new File("./src/main/resources/assets/"));
         delete(new File("./src/main/resources/data/lightland/loot_tables/"));
+        delete(new File("./src/main/resources/data/lightland/recipes/"));
         new LangFileOrganizer();
         new ItemFileOrganizer();
         new BlockFileOrganizer();
         new ArmorFileOrganizer();
+        new RecipeFileOrganizer();
         File f = new File("./src/test/resources");
         for (File fi : f.listFiles()) {
             MODID = fi.getName();
