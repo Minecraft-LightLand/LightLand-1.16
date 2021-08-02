d56efb056c2d0a85a781715afaf2b0f00e5f0d94
lcy0x1 <arthurlcy0x1@gmail.com>
2021-08-02 17:29:31 +0800

    add more profession icons

--- src/main/java/com/hikarishima/lightland/magic/profession/Profession.java
+++ src/main/java/com/hikarishima/lightland/magic/profession/Profession.java
@@ -3,6 +3,7 @@ package com.hikarishima.lightland.magic.profession;
 import com.hikarishima.lightland.magic.MagicRegistry;
 import com.hikarishima.lightland.magic.capabilities.MagicHandler;
 import com.lcy0x1.base.NamedEntry;
+import net.minecraft.util.ResourceLocation;
 
 public abstract class Profession extends NamedEntry<Profession> {
 
@@ -11,5 +12,10 @@ public abstract class Profession extends NamedEntry<Profession> {
     }
 
     public abstract void init(MagicHandler handler);
+
+    public ResourceLocation getIcon() {
+        ResourceLocation rl = getRegistryName();
+        return new ResourceLocation(rl.getNamespace(), "textures/profession/" + rl.getPath() + ".png");
+    }
 
 }
