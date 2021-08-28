8d6a5a1bfc6b5e1ae55629eca338ad091c8f06a6
tursom <tursom@foxmail.com>
2021-08-28 12:24:02 +0800

    update build script

--- build.ps1
+++ build.ps1
@@ -1,4 +1,9 @@
+mkdir release
 ./gradlew.bat lightland-core:reobfJar
+cp lightland-core/build/libs/*.jar release
 ./gradlew.bat lightland-magic:reobfJar
+cp lightland-magic/build/libs/*.jar release
 ./gradlew.bat lightland-quest:reobfJar
+cp lightland-quest/build/libs/*.jar release
 ./gradlew.bat lightland-terrain:reobfJar
+cp lightland-terrain/build/libs/*.jar release
