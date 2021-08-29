f911a45a730cc1f41a6532711dccbb44f3d9c51f
tursom <tursom@foxmail.com>
2021-08-29 17:34:30 +0800

    add proxy block usage config

--- lightland-core/src/main/java/com/lcy0x1/base/block/BaseBlock.java
+++ lightland-core/src/main/java/com/lcy0x1/base/block/BaseBlock.java
@@ -1,6 +1,7 @@
 package com.lcy0x1.base.block;
 
 import com.lcy0x1.base.block.type.IImpl;
+import lombok.extern.log4j.Log4j2;
 import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.block.Block;
 
@@ -8,9 +9,14 @@ import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
+@Log4j2
 public class BaseBlock extends Block {
+    private static final boolean defaultUseProxy = false;
+    private static final boolean useProxy = Boolean.parseBoolean(System.getProperty("lightland.proxy.block", Boolean.toString(defaultUseProxy)));
 
-    private static final boolean useProxy = false;
+    static {
+        log.info("use proxy block: {}", System.getProperty("lightland.proxy.block"));
+    }
 
     public static BaseBlock newBaseBlock(BlockProp p, IImpl... impl) {
         return useProxy ? ProxyBaseBlock.newBaseBlock(p, impl) : new ImplBaseBlock(p, impl);
