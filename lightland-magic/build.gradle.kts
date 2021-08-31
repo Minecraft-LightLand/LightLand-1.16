d751c466df8348e2da237a21bad0c49b9f15a5b4
tursom <tursom@foxmail.com>
2021-08-31 08:57:47 +0800

    add libs dependencies

--- lightland-magic/build.gradle.kts
+++ lightland-magic/build.gradle.kts
@@ -29,7 +29,7 @@ configureForge {
 useGeneratedResources()
 
 dependencies {
-    implementation(fileTree(mapOf("dir" to "libs", "include" to arrayOf("*.jar"))))
+    compileOnly(fg.deobf(fileTree(mapOf("dir" to "libs", "include" to arrayOf("*.jar")))))
     minecraft(project)
     core
     lombok
