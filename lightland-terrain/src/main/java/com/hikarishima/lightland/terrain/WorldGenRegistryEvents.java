55a87248c2c6582aeda14b25f193562e4eb5fbf5
lcy0x1 <arthurlcy0x1@gmail.com>
2021-08-31 12:43:47 +0800

    fix terrain

--- lightland-terrain/src/main/java/com/hikarishima/lightland/terrain/WorldGenRegistryEvents.java
+++ lightland-terrain/src/main/java/com/hikarishima/lightland/terrain/WorldGenRegistryEvents.java
@@ -1,6 +1,6 @@
 package com.hikarishima.lightland.terrain;
 
-import com.hikarishima.lightland.registry.RegistryBase;
+import com.lcy0x1.core.util.ExceptionHandler;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.placement.Placement;
@@ -10,6 +10,10 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.IForgeRegistryEntry;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.function.Consumer;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 @SuppressWarnings("unused")
@@ -39,13 +43,26 @@ public class WorldGenRegistryEvents {
 
     public static <T extends IForgeRegistryEntry<T>> void processBiome(RegistryEvent.Register<T> event) {
         for (Class<?> cls : BIOME_REGISTRIES)
-            RegistryBase.process(cls, event.getRegistry().getRegistrySuperType(), event.getRegistry()::register);
+            process(cls, event.getRegistry().getRegistrySuperType(), event.getRegistry()::register);
     }
 
 
     @SubscribeEvent
     public static void onWorldTypeRegistry(RegistryEvent.Register<ForgeWorldType> event) {
         event.getRegistry().register(LightLandTerrain.WORLD_TYPE.setRegistryName(LightLandTerrain.MODID, "image_biome"));
+    }
+    
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public static <T> void process(Class<?> provider, Class<T> reg, Consumer<T> acceptor) {
+        ExceptionHandler.run(() -> {
+            for (Field f : provider.getDeclaredFields())
+                if ((f.getModifiers() & Modifier.STATIC) != 0)
+                    if (reg.isAssignableFrom(f.getType()))
+                        ((Consumer) acceptor).accept(f.get(null));
+                    else if (f.getType().isArray() && reg.isAssignableFrom(f.getType().getComponentType()))
+                        for (Object o : (Object[]) f.get(null))
+                            ((Consumer) acceptor).accept(o);
+        });
     }
 
 }
