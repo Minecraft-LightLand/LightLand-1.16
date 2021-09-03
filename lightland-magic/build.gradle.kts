d4c8d9cef2736fa4d9edb97cac288af3489e742c
lcy0x1 <arthurlcy0x1@gmail.com>
2021-09-04 07:09:16 +0800

    add patchouli dependency

--- lightland-magic/build.gradle.kts
+++ lightland-magic/build.gradle.kts
@@ -26,11 +26,17 @@ repositories {
 configureForge {
     runs {
         createClient("clientMagic", project) {
-            properties(mapOf("mixin.env.disableRefMap" to "true"))
+            properties(mapOf(
+                    "mixin.env.remapRefMap" to "true",
+                    "mixin.env.refMapRemappingFile" to "${buildDir}/createSrgToMcp/output.srg"
+            ))
             arg("-mixin.config=lightland-magic.mixins.json")
         }
         createServer("serverMagic", project) {
-            properties(mapOf("mixin.env.disableRefMap" to "true"))
+            properties(mapOf(
+                    "mixin.env.remapRefMap" to "true",
+                    "mixin.env.refMapRemappingFile" to "${buildDir}/createSrgToMcp/output.srg"
+            ))
             arg("-mixin.config=lightland-magic.mixins.json")
         }
     }
@@ -54,7 +60,7 @@ dependencies {
     //runtimeOnly(fg.deobf("zip.local.citadel:citadel-1.7.3-1.16.5:1.7.3"))
     //compileOnly(fg.deobf("zip.local.tf:twilightforest-1.16.5:4.0.546-universal"))
     runtimeOnly(fg.deobf("zip.local.tf:twilightforest-1.16.5:4.0.546-universal"))
-    //runtimeOnly(fg.deobf("zip.local.iaf:iceandfire-2.1.8-1.16.5:2.1.8"))
+    //runtimeOnly(fg.deobf("zip.local.iaf:iceandfire-2.1.9-1.16.5:2.1.9"))
     //runtimeOnly(fg.deobf("zip.local.dg:dungeons_gear-1.16.5:3.0.17"))
     //compileOnly(fg.deobf("zip.local.mt:mahoutsukai-1.16.5:v1.31.37"))
     runtimeOnly(fg.deobf("zip.local.mt:mahoutsukai-1.16.5:v1.31.37"))
